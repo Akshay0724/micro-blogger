e1e2d05d4289c8ac216aa920f40ca59a7f8942a6
Areeb Jamal <jamal.areeb@gmail.com>
2018-07-13 16:58:59 +0530

    feat: Add created_at to Blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User 
-from django.shortcuts import get_object_or_404
-# Create your models here.
+from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     content = models.TextField()
