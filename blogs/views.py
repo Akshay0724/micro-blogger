37444ebadd33b693f35af2650a98ae53e33fa4f8
akshay0724 <akshay0724@gmail.com>
2018-07-13 13:05:09 +0530

    feat: Add blog detail (#5)

--- blogs/views.py
+++ blogs/views.py
@@ -5,3 +5,8 @@ from . import models
 def index(request):
 	blogs = models.Blog.objects.all()
 	return render(request, 'blogs/index.html', {'blogs': blogs})
+
+
+def blog(request,blog_id):
+	blog = models.Blog.objects.get(pk=blog_id)
+	return render(request, 'blogs/post_detail.html', {'blog': blog})
