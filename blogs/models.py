3e028e36a837ac07efd111fe260c2e1d6e5bc414
Areeb Jamal <jamal.areeb@gmail.com>
2018-07-16 02:02:22 +0530

    fix: Change created_at and add updated_at

--- blogs/models.py
+++ blogs/models.py
@@ -6,7 +6,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -16,7 +17,8 @@ class Comment(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def border_tag(self):
